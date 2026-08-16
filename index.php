003aa477e2a5f725a05065838dd3615f9f508d78
coriander67 <coriander6781@gmail.com>
2026-08-16 19:13:36 +1200

    login system

--- index.php
+++ index.php
@@ -1,13 +1,34 @@
 <?php
+include 'db.php';
 session_start();
 if ($_SESSION['username']==NULL){
 	$_SESSION['username']= $_POST['username'];
 }
 if ($_SESSION['username']!==$_POST['username'] && $_POST['username']!==NULL){
-	$_SESSION['username']=$_POST['username'];
+	$_SESSION['username']= $_POST['username'];
 }
+
+//if (($_SESSION['passcode']==NULL))
+
+$username= $_SESSION['username'];
+if ($_POST['passcode']!=NULL){
+	$_SESSION['hashpass']=hash('sha256',$_POST['passcode']);
+}
+
+$data=[
+	'username' => $_SESSION['username']
+];
+$stmt = $pdo->prepare("SELECT password FROM users WHERE username=:username");
+$stmt ->execute(['username' => $_SESSION['username']]);
+$rp = $stmt->fetchColumn();
+//$rp = "SELECT password FROM users WHERE username=$username";
+
+if ($_SESSION['hashpass']!=$rp){
+	header('Location: http://cori.dpdns.org/login.php');
+	die();
+}
+
 date_default_timezone_set("Pacific/Auckland");
-include 'db.php';
 $time= date('Y-m-d H:i:s a');
 $stmt= $pdo->query('SELECT * FROM employees');
 #while($row = $stmt-> fetch(PDO::FETCH_ASSOC)){
@@ -24,8 +45,8 @@ $stmt= $pdo->query('SELECT * FROM employees');
 echo "---------------------".$row['user'].'<br>';
 echo $row['time']."--".$row['message'].'<br>';}?>
 </h3>
-<h1 style="color:white;">step into the database<br> hn<br> the time is:
-<?php echo $time." (in nz)<br>- You are ".$_SESSION['username']?> 
+<h1 style="color:white;">welcome to the database<br> the time is:
+<?php echo $time." (in nz)<br>- You are ".$_SESSION['username']."<br>".$rp."<br>".$_SESSION['hashpass']?> 
 </h1>
 <br>
 <form action = "insert.php" method="post" ><input type="text" name="comment"></form>
--- login.php
+++ login.php
@@ -1,5 +1,14 @@
 <?php
-
+include 'db.php';
+session_start();
 ?>
+<head><link rel=stylesheet href="styles.css"></head>
 <body>enter login info</body>
-<form action="index.php" method="post"><input type="text" name="username"></form>
+<form action="index.php" method="post">
+user<br>
+<input type="text" name="username">
+<br>pass<br>
+<input type="password" name="passcode">
+<br>
+<input type="submit">
+</form>
